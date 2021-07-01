2fedeacb949156b810309fa4da8f136749e271b9
hqahsanul <hqahsanul@gmail.com>
2021-07-01 12:29:42 +0530

    Added env to hide secret

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
@@ -37,8 +39,10 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-const secret = "Thisissecret";
-userSchema.plugin(encrypt, { secret: secret, encryptField: ["password"] });
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ["password"],
+});
 
 const User = new mongoose.model("User", userSchema);
 
