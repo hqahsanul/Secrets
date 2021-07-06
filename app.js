bd868f4547ea26140a44683051bc67447cd1f92b
hqahsanul <hqahsanul@gmail.com>
2021-07-06 14:46:42 +0530

    added bcrypt and saltRound to password

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const { userInfo } = require("os");
-const encrypt = require("mongoose-encryption");
-const md5 = require("md5");
-
+const bcrypt=require("bcrypt")
+const saltRounds=10;
 const app = express();
 
 app.set("view engine", "ejs");
@@ -43,34 +41,40 @@ const userSchema = new mongoose.Schema({
 const User = new mongoose.model("User", userSchema);
 
 app.post("/register", function (req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password),
-  });
-  newUser.save(function (err) {
-    if (!err) {
-      res.render("secrets");
-    } else {
-      console.log(err);
-    }
-  });
+ 
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    
+    const newUser = new User({
+      email: req.body.username,
+      password: hash,
+    });
+    newUser.save(function (err) {
+      if (!err) {
+        res.render("secrets");
+      } else {
+        console.log(err);
+      }
+    });
+});
+ 
+
 });
 
 //implementing login page
 
 app.post("/login", function (req, res) {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
   User.findOne({ email: username }, function (err, foundUser) {
     if (err) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        } else {
-          console.log("Invalid password");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if(result===true){
+            res.render("secrets")
+          }
+      });
       }
     }
   });
